c2ff9b2c1c3b882b366cf006c99c9428fff3fe9c
Luiz Felipe <alucardlf1911@gmail.com>
2025-09-16 16:39:41 -0300

    Criando o Projeto

---
+++ APROVADOR_ALUNOS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace APROVADOR_ALUNOS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
