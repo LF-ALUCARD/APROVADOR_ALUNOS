c6822b857c00470b94c8bc5bad94a7cfcb2bb864
Luiz Felipe <alucardlf1911@gmail.com>
2025-09-18 14:10:00 -0300

    Criando a classe Lista

---
+++ APROVADOR_ALUNOS/services/Lista.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APROVADOR_ALUNOS.services
+{
+    internal class Lista<T>
+    {
+        private No<T> inicio;
+        private int tamanho;
+    
+        public Lista()
+        {
+            inicio = null;
+            tamanho = 0;
+        }
+
+        public int getTamanho() { return tamanho; }
+
+    }
+}
