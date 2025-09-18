2a8bc8b86953f94059b5c782a29125bda09bb58c
Luiz Felipe <alucardlf1911@gmail.com>
2025-09-18 12:34:16 -0300

    Criando a classe No

---
+++ APROVADOR_ALUNOS/services/No.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APROVADOR_ALUNOS.services
+{
+    internal class No<T>
+    {
+        private T elemento;
+        private No<T> no;
+
+        public No(T elemento)
+        {
+            this.elemento = elemento;
+            this.no = null;
+        }
+
+        public No(T elemento, No<T> no)
+        {
+            this.elemento = elemento;
+            this.no = no;
+        }
+
+        public T getElemento() { return elemento; }
+        public void setElemento(T elemento) { this.elemento = elemento; }
+
+        public No<T> getNo() { return no; }
+        public void setNo(No<T> no) { this.no = no; }
+    }
+}
